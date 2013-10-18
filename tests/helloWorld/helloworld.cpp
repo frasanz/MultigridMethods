52fe0c2fa1f5933d0e535014427586b5dfb8b8c5
Francisco Sanz Garcia <francisco.sanz.g@gmail.com>
2013-10-18 18:37:27 +0200

    Some tests with git

---
+++ tests/helloWorld/helloworld.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:  Simple hello world
+ *
+ *        Version:  1.0
+ *        Created:  18/10/13 18:18:33
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Francisco Sanz Garcia (fsg), francisco.sanz.g@gmail.com
+ *        Company:  own
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	std::cout << "Hello world!" << endl;
+	return 0;
+}
