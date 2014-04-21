2ee10d1a156baede6518c12f1a6b803e31e3d9d3
spac <frasanz@bifi.es>
2014-04-21 22:13:55 +0200

    Testing vector grid

---
+++ tests/vector_grid/vector_grid.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vector_grid.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  21/04/14 22:10:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+  return 0;
+}
