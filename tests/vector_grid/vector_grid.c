16363ea6986389bb2640ff0a880babfa143d6fac
spac <frasanz@bifi.es>
2014-04-21 22:28:17 +0200

    Separating files

--- tests/vector_grid/vector_grid.c
+++ tests/vector_grid/vector_grid.c
@@ -18,8 +18,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include "vector_grid.h"
 
 int main(){
+  /* Definitions */
+  Grid g1;
+
+  /* Auxiliary definitions */
+  int i;
+
+  /* Malloc */
+  g1.u1 = (double*) malloc (SIZE*sizeof(double));
+  g1.u2 = (double*) malloc (SIZE*sizeof(double));
+  g1.u3 = (double*) malloc (SIZE*sizeof(double));
+
+  for(i=0; i<SIZE; i++){
+    g1.u1[i]=rand();
+    g1.u2[i]=rand();
+    g1.u3[i]=rand();
+  }
+
+
+  /* Free */
+  free(g1.u1);
+  free(g1.u2);
+  free(g1.u3);
+
+
   return 0;
 }
---
+++ tests/vector_grid/vector_grid.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vector_grid.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  21/04/14 22:26:30
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef _VECTOR_GRID_H_
+#define _VECTOR_GRID_H_
+
+#define SIZE 2048
+
+typedef struct{
+  double * u1;
+  double * u2;
+  double * u3;
+} Grid;
+
+#endif
